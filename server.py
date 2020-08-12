42f77f186e01d8d41c604ae1382356dce7c8ec7c
antauren <antauren@yandex.ru>
2020-08-12 03:57:19 +0300

    add server

---
+++ server.py
@@ -0,0 +1,100 @@
+import os
+
+from dotenv import load_dotenv
+
+from aiohttp import web
+
+from jsonrpc import JSONRPCResponseManager, dispatcher
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine, Float, Boolean
+
+base = declarative_base()
+
+
+class Account(base):
+    __tablename__ = 'account'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    amount = Column(Float, default=0)
+    overdraft = Column(Boolean)
+
+    def __repr__(self):
+        return '<Account {} {}>'.format(self.id, self.name)
+
+
+def get_session(db, echo=False):
+    engine = create_engine(db, echo=echo)
+
+    base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    return session
+
+
+def create_account(name: str, overdraft: bool, amount=0):
+    account = Account(name=name, overdraft=overdraft, amount=amount)
+
+    session.add(account)
+    session.commit()
+
+    return account.id
+
+
+def transfer_money(donor_id: int, recipient_id: int, amount: int):
+    donor = session.query(Account).get(donor_id)
+    recipient = session.query(Account).get(recipient_id)
+
+    if (not donor.overdraft) and (donor.amount - amount < 0):
+        return False
+
+    donor.amount -= amount
+    recipient.amount += amount
+
+    session.commit()
+
+    return True
+
+
+def get_balance(account_id: int):
+    account = session.query(Account).get(account_id)
+
+    return account.amount
+
+
+async def handle_jsonrpc(request):
+    dispatcher['create_account'] = create_account
+    dispatcher['transfer_money'] = transfer_money
+    dispatcher['get_balance'] = get_balance
+
+    request_dict = await request.json()
+    response = JSONRPCResponseManager.handle(request_dict, dispatcher)
+
+    return web.json_response(response.json)
+
+
+def is_true(value: str) -> bool:
+    return True if value.lower() == 'true' else False
+
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    session = get_session(
+        db=os.getenv('DATABASE'),
+        echo=is_true(os.getenv('DATABASE_DEBUG', '')),
+    )
+
+    app = web.Application()
+    app.add_routes([
+        web.post('/jsonrpc', handle_jsonrpc),
+    ])
+
+    web.run_app(app,
+                host=os.getenv('SERVER_HOST'),
+                port=os.getenv('SERVER_PORT'),
+                )
