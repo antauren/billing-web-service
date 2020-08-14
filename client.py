020e6a7c6af8868bad65b53e7985b92005cfbeb7
antauren <antauren@yandex.ru>
2020-08-14 04:08:06 +0300

    check jsonrpc version

--- client.py
+++ client.py
@@ -4,6 +4,11 @@ import requests
 def main():
     url = 'http://localhost:8080/jsonrpc'
 
+    # jsonrpc version error
+    payload = {'jsonrpc': '1.0'}
+    response = requests.post(url, json=payload).json()
+    assert response['error']['code'] == -32000
+
 
 if __name__ == "__main__":
     main()
--- heandlers.py
+++ heandlers.py
@@ -1,2 +1,12 @@
 def heandle_request(request) -> dict:
+    if not is_jsonrpc_version_true(request):
+        return {'jsonrpc': '2.0',
+                'error': {'code': -32000, 'message': 'The server supports only "jsonrpc 2.0" version request.'},
+                'id': request.get('id', None)
+                }
+
     return {}
+
+
+def is_jsonrpc_version_true(request):
+    return request.get('jsonrpc', None) == '2.0'
