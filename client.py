742b2af3c34d77c36d688b25079c06de9a3252f3
antauren <antauren@yandex.ru>
2020-08-17 08:06:32 +0300

    reformat

--- client.py
+++ client.py
@@ -16,39 +16,44 @@ def main():
     assert response['error']['code'] == -32700
 
     # Invalid Request
-    payload = json.dumps({'jsonrpc': '2.0', 'method': None})
-    response = requests.post(url, data=payload).json()
+    payload = {'method': None, 'jsonrpc': '2.0'}
+    response = requests.post(url, json=payload).json()
     assert response['error']['code'] == -32600
 
     # Invalid Request
-    payload = json.dumps({'jsonrpc': '2.0', 'method': 'foo', 'params': -1})
-    response = requests.post(url, data=payload).json()
+    payload = {'method': 'foo', 'params': -1, 'jsonrpc': '2.0'}
+    response = requests.post(url, json=payload).json()
     assert response['error']['code'] == -32600
 
     # Method not found
-    payload = json.dumps({'jsonrpc': '2.0', 'method': 'abcdefghijklmnopqrstuvwxyz'})
-    response = requests.post(url, data=payload).json()
+    payload = {'method': 'abcdefghijklmnopqrstuvwxyz', 'jsonrpc': '2.0'}
+    response = requests.post(url, json=payload).json()
     assert response['error']['code'] == -32601
 
     # Invalid params (params count)
-    payload = json.dumps({'jsonrpc': '2.0', 'method': 'create_account', 'params': ['name_1', False, 0] * 100})
-    response = requests.post(url, data=payload).json()
+    payload = {'method': 'create_account', 'params': ['name_1', False, 0] * 100, 'jsonrpc': '2.0'}
+    response = requests.post(url, json=payload).json()
     assert response['error']['code'] == -32602
 
     # transfer_money
-    donor_id = requests.post(url, json={'jsonrpc': '2.0', 'method': 'create_account', 'params': ['name_1', True]}
+    donor_id = requests.post(url,
+                             json={'method': 'create_account', 'params': ['name_1', True], 'jsonrpc': '2.0', }
                              ).json()['result']
-    recipient_id = requests.post(url, json={'jsonrpc': '2.0', 'method': 'create_account', 'params': ['name_2', True]}
+    recipient_id = requests.post(url,
+                                 json={'method': 'create_account', 'params': ['name_2', True], 'jsonrpc': '2.0'}
                                  ).json()['result']
-    requests.post(url, json={'jsonrpc': '2.0', 'method': 'transfer_money', 'params': [donor_id, recipient_id, 300]}
+    requests.post(url,
+                  json={'method': 'transfer_money', 'params': [donor_id, recipient_id, 300], 'jsonrpc': '2.0'}
                   )
-    donor_amount = requests.post(url, json={'jsonrpc': '2.0', 'method': 'get_balance', 'params': [donor_id]}
+    donor_amount = requests.post(url,
+                                 json={'method': 'get_balance', 'params': [donor_id], 'jsonrpc': '2.0'}
                                  ).json()['result']
-    recipient_amount = requests.post(url, json={'jsonrpc': '2.0', 'method': 'get_balance', 'params': [recipient_id]}
+    recipient_amount = requests.post(url,
+                                     json={'method': 'get_balance', 'params': [recipient_id], 'jsonrpc': '2.0'}
                                      ).json()['result']
     assert donor_amount == -300.0
     assert recipient_amount == 300.0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
